163029b87da9c1b617131d1bffaf890622a5edf4
Justin Sijbolts <partydoos@gmail.com>
2023-01-19 12:27:54 +0100

    Finish Script
    Player can now finish the game.

--- Assets/Scripts/finishScript.cs
+++ Assets/Scripts/finishScript.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.UI;
+
+
 public class finishScript : MonoBehaviour
 {
     // Start is called before the first frame update
     pickupScript pickupScript;
     bool playerWon;
+    bool playerLost;
+
+    GameObject StatusEndGame;
+
+    Text StatusHeader;
+    Text StatusText;
     void Start()
     {
         pickupScript = GameObject.Find("playerCapsule").GetComponent<pickupScript>();
+        StatusEndGame = GameObject.Find("StatusEndGame");
     }
 
     // Update is called once per frame
@@ -18,6 +28,20 @@ public class finishScript : MonoBehaviour
         if (pickupScript.pickupsPicked == pickupScript.maxPickups)
         {
             playerWon = true;
+        }
+
+        if (playerWon && StatusEndGame.transform.localScale != new Vector3(1, 1, 1))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+
+            StatusEndGame.transform.localScale = new Vector3(1, 1, 1);
+            StatusHeader = GameObject.Find("StatusHeader").GetComponent<Text>();
+            StatusText = GameObject.Find("StatusText").GetComponent<Text>();
+            StatusHeader.text = "You Won!";
+
+            StatusText.text = "You've removed all the trash from this town!";
         }
     }
 }
