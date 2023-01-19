32879a79770d2daece1df96cece72609adb299ab
RiemerHF <55958573+RiemerHF@users.noreply.github.com>
2023-01-19 11:22:23 +0100

    Pickup

--- Assets/Scripts/pickupScript.cs
+++ Assets/Scripts/pickupScript.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class pickupScript : MonoBehaviour
 {
+    public int pickups;
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Collectable")
+        {
+            pickups++;
+            Destroy(col.gameObject);
+        }
     }
 
     // Update is called once per frame
