c636890f2612699586d5b6c85cc38099ebd351ab
Justin Sijbolts <partydoos@gmail.com>
2023-01-19 11:08:44 +0100

    Added Main Water Script base

--- Assets/Scripts/mainWaterScript.cs
+++ Assets/Scripts/mainWaterScript.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class mainWaterScript : MonoBehaviour
 {
+    characterControllerScr characterControllerScr;
+    bool hasReachedWater;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterControllerScr = GameObject.Find("playerCapsule").GetComponent<characterControllerScr>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0.0005f, 0);
+        transform.Translate(0, 0.05f * Time.deltaTime, 0);
+        
+        if (transform.position.y > -101.07f && !hasReachedWater)
+        {
+            characterControllerScr.speed -= 1f;
+            Debug.Log("Water has reached level surface");
+            hasReachedWater = true;
+        }
+
+        if (transform.position.y > -101.5f && characterControllerScr.speed > 0)
+        {
+            characterControllerScr.speed -= 0.5f * Time.deltaTime;
+        }
     }
 }
