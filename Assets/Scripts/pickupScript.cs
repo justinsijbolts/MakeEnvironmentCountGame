1e33f7997a73f48be74e4c81f6143e3e97a70b40
Justin Sijbolts <partydoos@gmail.com>
2023-01-19 12:14:20 +0100

    Added Progress Bar

--- Assets/Scripts/pickupScript.cs
+++ Assets/Scripts/pickupScript.cs
@@ -4,26 +4,37 @@ using UnityEngine;
 
 public class pickupScript : MonoBehaviour
 {
-    public int pickups;
+    public float pickupsPicked;
+    GameObject PickupBar;
+    private float maxPickups;
+    bool pickupsCounted;
+
+    void Start() {
+        PickupBar = GameObject.Find("PP_ANCHOR");
+
+        foreach (GameObject pickup in GameObject.FindGameObjectsWithTag("Collectable"))
+        {
+            maxPickups++;
+        }
+    }
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void ProgressBarUpdate() {
+        // 0 Pickups picked scale = 0. All pickups picked scale = 1
+        //(6 / 12) x 100 = 50
+
+        float scale = pickupsPicked / maxPickups;
+        PickupBar.transform.localScale = new Vector3(scale, 1, 1);
     }
 
     public void OnTriggerEnter(Collider col)
     {
         if(col.gameObject.tag == "Collectable")
         {
-            pickups++;
+            pickupsCounted = true;
+            pickupsPicked++;
             Destroy(col.gameObject);
+            ProgressBarUpdate();
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
